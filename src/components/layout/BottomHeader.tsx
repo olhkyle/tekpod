3fc4f22aa3c4a78c056b40499975ee8a75807d20
olhkyle <kylekwon.dev@gmail.com>
2024-08-16 13:27:01 +0900

    style:add Spacer Component on Mobile Environment

--- src/components/layout/BottomHeader.tsx
+++ src/components/layout/BottomHeader.tsx
@@ -6,40 +6,55 @@ import { routes } from '../../constants';
 
 const BottomHeader = () => {
 	const {
-		userInfo: { id },
 		authQuery: { data },
 	} = useAuthQuery();
 
 	return (
 		<Header>
-			<NavLink href={routes.HOME}>
-				<RiHome6Line size={24} />
-			</NavLink>
-			<NavLink href={routes.DIARY}>
-				<RiBook3Line size={24} />
-			</NavLink>
-			<NavLink href={routes.WRITE}>
-				<RiPencilLine size={26} />
-			</NavLink>
-			<NavLink href={data ? `${routes.USER}/${id}` : routes.LOGIN}>
-				<RiUser3Line size={24} />
-			</NavLink>
+			<Links>
+				<NavLink href={routes.HOME}>
+					<RiHome6Line size={24} />
+				</NavLink>
+				<NavLink href={routes.DIARY}>
+					<RiBook3Line size={24} />
+				</NavLink>
+				<NavLink href={routes.WRITE}>
+					<RiPencilLine size={26} />
+				</NavLink>
+				<NavLink href={data ? `${routes.USER}/${data.id}` : routes.LOGIN}>
+					<RiUser3Line size={24} />
+				</NavLink>
+			</Links>
+			<Spacer />
 		</Header>
 	);
 };
 
 const Header = styled.header`
-	display: flex;
-	justify-content: space-around;
-	align-items: center;
 	position: fixed;
 	bottom: 0;
 	max-width: var(--max-app-width);
 	min-width: var(--min-app-width);
 	width: 100%;
-	height: var(--nav-height);
 	background-color: var(--white);
 	border-top: 1px solid var(--grey200);
+`;
+
+const Links = styled.div`
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	width: 100%;
+	height: var(--nav-height);
+`;
+
+const Spacer = styled.div`
+	display: block;
+	height: 16px;
+
+	@media screen and (min-width: 640px) {
+		display: none;
+	}
 `;
 
 export default BottomHeader;
