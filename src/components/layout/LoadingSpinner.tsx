6f46c206e813851fccc864f638ce857218f4dda5
olhkyle <kylekwon.dev@gmail.com>
2024-08-16 12:57:11 +0900

    ✨ feat:add loading spinner

---
+++ src/components/layout/LoadingSpinner.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+const LoadingSpinner = () => {
+	return (
+		<Container>
+			<img src="/eclipse-half.svg" alt="spinner" />
+		</Container>
+	);
+};
+
+const Container = styled.div`
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100dvw;
+	height: 100dvh;
+
+	img {
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate3d(-50%, -50%);
+	}
+`;
+
+export default LoadingSpinner;
