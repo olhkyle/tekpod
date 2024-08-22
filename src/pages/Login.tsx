0366f45c0168870fcf2f28393bc8fbd2b1c84e8d
olhkyle <kylekwon.dev@gmail.com>
2024-08-23 00:01:48 +0900

    refactor: refactor login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -8,6 +9,25 @@ import { loginSchema, type LoginSchema } from '../components/auth/schema';
 import useUserStore from '../store/userStore';
 import useLoading from '../hooks/useLoading';
 import { routes } from '../constants';
+
+const pageCss = {
+	container: css`
+		max-width: var(--max-app-width);
+		min-width: var(--min-app-width);
+		margin: 0 auto;
+		overflow: hidden;
+	`,
+	form: css`
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 16px;
+		padding: var(--padding-container-mobile);
+		height: 100dvh;
+		background-color: var(--white);
+	`,
+};
 
 const LoginPage = () => {
 	const {
@@ -32,44 +52,40 @@ const LoginPage = () => {
 				throw new Error(error.message);
 			}
 
-			setUserData(data.session);
-			navigate(routes.HOME);
+			if (data) {
+				console.log(data);
+				setUserData(data.session);
+				navigate(routes.HOME);
+			}
 		} catch (error) {
 			console.error(error);
 		}
 	};
 
 	return (
-		<Form onSubmit={handleSubmit(onSubmit)}>
-			<Title>﹡﹡</Title>
-			<LabelInput label={'email'} errorMessage={errors?.email?.message}>
-				<LabelInput.TextField type={'email'} id={'email'} {...register('email')} placeholder={'Email'} />
-			</LabelInput>
-			<LabelInput label={'password'} errorMessage={errors?.password?.message}>
-				<LabelInput.TextField type={'password'} id={'password'} {...register('password')} placeholder={'Password'} />
-			</LabelInput>
+		<div css={pageCss.container}>
+			<form css={pageCss.form} onSubmit={handleSubmit(onSubmit)}>
+				<Title>﹡﹡</Title>
+				<LabelInput label={'email'} errorMessage={errors?.email?.message}>
+					<LabelInput.TextField type={'email'} id={'email'} {...register('email')} placeholder={'Email'} />
+				</LabelInput>
+				<LabelInput label={'password'} errorMessage={errors?.password?.message}>
+					<LabelInput.TextField type={'password'} id={'password'} {...register('password')} placeholder={'Password'} />
+				</LabelInput>
 
-			<SubmitButton type="submit" aria-label="Login Button">
-				{isLoading ? Loading : 'LOGIN'}
-			</SubmitButton>
-			<ActionButtons>
-				<ResetPasswordButton type="button" aria-label="Reset Password Button">
-					Reset Password
-				</ResetPasswordButton>
-				<RegisterLink to={routes.REGISTER}>SignUp</RegisterLink>
-			</ActionButtons>
-		</Form>
+				<SubmitButton type="submit" aria-label="Login Button">
+					{isLoading ? Loading : 'LOGIN'}
+				</SubmitButton>
+				<ActionButtons>
+					<ResetPasswordButton type="button" aria-label="Reset Password Button">
+						Reset Password
+					</ResetPasswordButton>
+					<RegisterLink to={routes.REGISTER}>SignUp</RegisterLink>
+				</ActionButtons>
+			</form>
+		</div>
 	);
 };
-
-const Form = styled.form`
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	gap: 16px;
-	height: calc(100dvh - 2.5 * var(--nav-height));
-`;
 
 const Title = styled.h2`
 	min-width: 270px;
