b6e9ee0f6afe32c778e02d086656dac32c376582
olhkyle <kylekwon.dev@gmail.com>
2024-08-22 16:40:10 +0900

    feat: add suspense fallback on DiaryPage

--- src/pages/Diary.tsx
+++ src/pages/Diary.tsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
-import { Content } from '../components';
+import { Content, ContentLoader } from '../components';
 import { Suspense } from 'react';
 
 const DiaryPage = () => {
 	return (
 		<section>
 			<Title>💿 Records Of My Life</Title>
-			<Suspense fallback={<div>Loading...</div>}>
+			<Suspense fallback={<ContentLoader />}>
 				<Content />
 			</Suspense>
 		</section>
