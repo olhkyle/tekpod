59314bb972269079cf40c69ee69a2bf3dbf109d3
olhkyle <kylekwon.dev@gmail.com>
2025-01-06 17:48:45 +0900

    feat : priceUnit 상수 추가

--- src/constants/financialLedger.ts
+++ src/constants/financialLedger.ts
@@ -1,10 +1,18 @@
 type PaymentDataType = (typeof paymentData)[keyof typeof paymentData][number];
 
+type PriceUnitType = (typeof priceUnit)['unitType'][number];
+type PriceUnitSymbolType = (typeof priceUnit)['unitSymbol'][number];
+
 const paymentData = {
 	paymentMethod: ['Card', 'Cash'] as const,
 	banks: ['신한', '하나', '국민', '우리', 'IBK기업', '농협', '카카오뱅크', '토스뱅크', '새마을', 'SC제일', '씨티', '해당없음'] as const,
 	priceUnits: ['WON', 'USD', 'GBP', 'EUR', 'JPY'],
 } as const;
 
-export type { PaymentDataType };
-export { paymentData };
+const priceUnit = {
+	unitType: ['WON', 'USD', 'GBP', 'EUR', 'JPY'] as const,
+	unitSymbol: ['₩', '$', '£', '€', '¥'] as const,
+} as const;
+
+export type { PaymentDataType, PriceUnitType, PriceUnitSymbolType };
+export { paymentData, priceUnit };
