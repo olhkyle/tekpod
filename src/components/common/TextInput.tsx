842034e8696822536ed6533411ee8ef0ee4de7f1
olhkyle <kylekwon.dev@gmail.com>
2025-01-29 00:08:05 +0900

    feat : TextInput의 너비 조정

--- src/components/common/TextInput.tsx
+++ src/components/common/TextInput.tsx
@@ -89,7 +89,7 @@ const Message = styled.p`
 
 const Input = styled.input<{ name: string }>`
 	padding: var(--padding-container-mobile);
-
+	width: 100%;
 	font-size: var(--fz-h5);
 	font-weight: ${({ name }) => (name === 'title' ? 'var(--fw-semibold)' : 'var(--fw-regular)')};
 	color: var(--black);
