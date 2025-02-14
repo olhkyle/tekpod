d5fe6b453a3864a1327cbe31bb20fde51aefb3f7
olhkyle <kylekwon.dev@gmail.com>
2025-02-14 18:21:03 +0900

    feat : add Title on ExpenseTrackerUpcomingPage

--- src/pages/ExpenseTrackerUpcoming.tsx
+++ src/pages/ExpenseTrackerUpcoming.tsx
@@ -1,7 +1,20 @@
+import styled from '@emotion/styled';
 import { WorkInProgress } from '../components';
 
 const ExpenseTrackerUpcoming = () => {
-	return <WorkInProgress />;
+	return (
+		<div>
+			<Title>Upcoming</Title>
+			<WorkInProgress />
+		</div>
+	);
 };
+
+const Title = styled.h2`
+	margin-bottom: 16px;
+	font-size: var(--fz-h5);
+	font-weight: var(--fw-black);
+	color: var(--grey900);
+`;
 
 export default ExpenseTrackerUpcoming;
