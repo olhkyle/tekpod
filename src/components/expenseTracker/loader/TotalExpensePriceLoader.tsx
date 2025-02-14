043b37b357e8957747672425a29d89bd00a05031
olhkyle <kylekwon.dev@gmail.com>
2025-02-14 12:09:01 +0900

    feat : Suspense와 useSuspenseQuery를 활용한 Loading 상태 표현을 위해TotalExpensePrice 컴포넌트 분리

---
+++ src/components/expenseTracker/loader/TotalExpensePriceLoader.tsx
@@ -0,0 +1,7 @@
+import { SkeletonLoader } from '../../common';
+
+const TotalExpensePriceLoader = () => {
+	return <SkeletonLoader width={'270px'} height={'48px'} />;
+};
+
+export default TotalExpensePriceLoader;
