bdf73a7dc30a7f2c1e8e786637e98dfe19682108
olhkyle <kylekwon.dev@gmail.com>
2025-02-19 15:39:39 +0900

    feat : add todayLocalString based on specific timeline

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,16 +1,17 @@
 const today = new Date();
+const todayLocaleString = today.toLocaleString('en-US', { timeZone: 'Asia/Seoul' });
 
 const [currentYear, currentMonth, currentDate] = [today.getFullYear(), today.getMonth(), today.getDate()];
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'] as const;
 
-const format = (targetDate: Date) => {
+const format = (targetDate: Date): string => {
 	const _date = new Date(targetDate);
 	const [year, month, date] = [_date.getFullYear(), _date.getMonth() + 1, _date.getDate()];
 
 	return `${year + ''}.${(month + '').padStart(2, '0')}.${(date + '').padStart(2, '0')}`;
 };
 
-const translateNumberIntoMonth = (month: number) => months[month];
+const translateNumberIntoMonth = (month: number) => months[month]; // ['Jan', 'Feb', 'Mar'][number]
 
-export { today, currentYear, currentMonth, currentDate, months, format, translateNumberIntoMonth };
+export { today, todayLocaleString, currentYear, currentMonth, currentDate, months, format, translateNumberIntoMonth };
