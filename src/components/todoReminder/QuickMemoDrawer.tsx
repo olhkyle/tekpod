03b47461ad1ff40677f0f448a2ef6f26cb0f2cac
olhkyle <kylekwon.dev@gmail.com>
2025-02-20 20:24:53 +0900

    feat: develop QuickMemoDrawer UI with Drawer Component

--- src/components/todoReminder/QuickMemoDrawer.tsx
+++ src/components/todoReminder/QuickMemoDrawer.tsx
@@ -1,15 +1,34 @@
+import styled from '@emotion/styled';
+
 import useDrawerStore from '../../store/useDrawerStore';
-import { Drawer } from '../common';
+import { Button, Drawer, TextInput } from '../common';
 
 const QuickMemoDrawer = () => {
 	const { isOpen, close } = useDrawerStore();
 
 	return (
-		<Drawer position={'top'} isOpen={isOpen} close={close}>
-			<div>what do you want to add?</div>
-			<button type="button">Submit</button>
-		</Drawer>
+		<>
+			{isOpen && (
+				<Drawer position={'top'} isOpen={isOpen} close={close} title={'Quick Memo'}>
+					<TextInput>
+						<TextInput.TextField id="memo" name="memo" placeholder="Something"></TextInput.TextField>
+					</TextInput>
+					<SubmitButton type="submit">Submit</SubmitButton>
+				</Drawer>
+			)}
+		</>
 	);
 };
+
+const SubmitButton = styled(Button)`
+	margin-top: 16px;
+	padding: var(--padding-container-mobile);
+	width: 100%;
+	min-height: 40px;
+	color: var(--white);
+	background-color: var(--black);
+	font-size: var(--fz-p);
+	font-weight: var(--fw-semibold);
+`;
 
 export default QuickMemoDrawer;
