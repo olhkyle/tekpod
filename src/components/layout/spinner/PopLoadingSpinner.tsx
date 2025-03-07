f4ff8bb68873c15960dee3328c53d3ec40147f0c
olhkyle <kylekwon.dev@gmail.com>
2025-03-07 17:17:58 +0900

    feat : add PopLoadingSpinner component

---
+++ src/components/layout/spinner/PopLoadingSpinner.tsx
@@ -0,0 +1,5 @@
+const PopLoadingSpinner = () => {
+	return <img src="/ellipsis-rotate.svg" alt="pop loading spinner" css={{ display: 'inline-block', width: '18px', height: '18px' }} />;
+};
+
+export default PopLoadingSpinner;
