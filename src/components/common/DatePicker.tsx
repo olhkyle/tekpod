208f9d4e86767196db8f4f32b730566f713d8e5c
olhkyle <kylekwon.dev@gmail.com>
2025-03-13 19:00:08 +0900

    feat : set dynamic DatePicker's position based on`isFloated` prop

--- src/components/common/DatePicker.tsx
+++ src/components/common/DatePicker.tsx
@@ -6,22 +6,24 @@ import { IoMdCalendar } from 'react-icons/io';
 import { ko } from 'date-fns/locale';
 import { Button } from '..';
 import { customPropReceiver } from '../../constants';
-import { format, today } from '../../utils/date';
+import { format } from '../../utils/date';
 import { useClickOutside } from '../../hooks';
 
 interface DatePickerProps {
 	selected: Date | undefined;
 	setSelected: (date: Date) => void | Dispatch<SetStateAction<Date | undefined>>;
 	error?: FieldError;
+	disabled?: { after: Date };
+	isFloated?: boolean;
 }
 
-const DatePicker = ({ selected, setSelected, error, ...props }: DatePickerProps) => {
+const DatePicker = ({ selected, setSelected, error, disabled, isFloated = false, ...props }: DatePickerProps) => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	const targetRef = useClickOutside<HTMLDivElement>({ eventHandler: () => setIsOpen(false) });
 
 	return (
-		<Container ref={targetRef}>
+		<Container ref={targetRef} isFloated={isFloated}>
 			<TriggerButton type="button" $isDaySelected={selected ? true : false} onClick={() => setIsOpen(!isOpen)} $isOpen={isOpen}>
 				<IconBackground>
 					<IoMdCalendar size="24" color="var(--grey800)" />
@@ -29,43 +31,45 @@ const DatePicker = ({ selected, setSelected, error, ...props }: DatePickerProps)
 				<span>{selected ? format(selected) : 'Select Date'}</span>
 			</TriggerButton>
 			{error && <ErrorMessage>﹡ {error?.message}</ErrorMessage>}
-			{isOpen && (
-				<DayPicker
-					mode="single"
-					locale={ko}
-					required={true}
-					selected={selected}
-					disabled={{ after: today }}
-					onSelect={setSelected}
-					captionLayout="dropdown"
-					timeZone="Asia/Seoul"
-					showOutsideDays
-					onDayClick={() => {
-						setIsOpen(false);
-					}}
-					{...props}
-				/>
-			)}
+			<DayPickerWrapper isFloated={isFloated}>
+				{isOpen && (
+					<DayPicker
+						mode="single"
+						locale={ko}
+						required={true}
+						selected={selected}
+						disabled={disabled}
+						onSelect={setSelected}
+						captionLayout="dropdown"
+						timeZone="Asia/Seoul"
+						showOutsideDays
+						onDayClick={() => {
+							setIsOpen(false);
+						}}
+						{...props}
+					/>
+				)}
+			</DayPickerWrapper>
 		</Container>
 	);
 };
 
-const Container = styled.div`
-	position: relative;
+const Container = styled.div<{ isFloated: boolean }>`
+	position: ${({ isFloated }) => (isFloated ? 'relative' : 'static')};
 	display: inline-flex;
 	flex-direction: column;
 	gap: 4px;
-	margin-top: 8px;
 `;
 
 const TriggerButton = styled(Button, customPropReceiver)<{ $isDaySelected: boolean; $isOpen: boolean }>`
 	display: inline-flex;
 	align-items: center;
 	gap: 6px;
-	padding: calc(var(--padding-container-mobile) * 0.5) var(--padding-container-mobile);
+	padding: calc(var(--padding-container-mobile) * 0.75) var(--padding-container-mobile);
+
 	color: ${({ $isDaySelected }) => ($isDaySelected ? 'var(--grey900)' : 'var(--grey500)')};
 	background-color: ${({ $isOpen }) => ($isOpen ? 'var(--white)' : 'var(--greyOpacity50)')};
-	border: 1px solid var(--grey100);
+	border: ${({ $isOpen }) => `1px solid ${$isOpen ? 'var(--grey300)' : 'var(--grey100)'}`};
 	border-radius: var(--radius-m);
 	font-size: var(--fz-p);
 	font-weight: var(--fw-semibold);
@@ -85,6 +89,11 @@ const ErrorMessage = styled.p`
 	padding-left: 4px;
 	font-size: var(--fz-sm);
 	color: var(--red200);
+`;
+
+const DayPickerWrapper = styled.div<{ isFloated: boolean }>`
+	position: ${({ isFloated }) => (isFloated ? 'absolute' : 'static')};
+	top: ${({ isFloated }) => (isFloated ? '64px' : '0')};
 `;
 
 export default DatePicker;
