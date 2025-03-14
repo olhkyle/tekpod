cd58b192ddde1351b9632481a843c1628892eb8b
olhkyle <kylekwon.dev@gmail.com>
2025-03-14 13:24:51 +0900

    feat : develop TodoListLoader

---
+++ src/components/todoReminder/TodoListLoader.tsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+import { SkeletonLoader } from '../common';
+
+const TodoListLoader = () => {
+	return (
+		<Container>
+			{Array.from({ length: 3 }, (_, idx) => (
+				<SkeletonLoader key={idx} theme={'light'} width={'100%'} height={'32px'} />
+			))}
+		</Container>
+	);
+};
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	margin-top: 32px;
+`;
+
+export default TodoListLoader;
--- src/components/todoReminder/index.ts
+++ src/components/todoReminder/index.ts
@@ -1,3 +1,4 @@
 export { default as TodoList } from './TodoList';
 export { default as TodoItem } from './TodoItem';
 export { default as QuickMemoDrawer } from './QuickMemoDrawer';
+export { default as TodoListLoader } from './TodoListLoader';
