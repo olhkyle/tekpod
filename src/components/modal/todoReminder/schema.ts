09311697d866924e3df4e947854048f6abb97d28
olhkyle <kylekwon.dev@gmail.com>
2025-03-14 19:06:33 +0900

    feat : add required_error of reminderTime on editTodoItemFormSchema

--- src/components/modal/todoReminder/schema.ts
+++ src/components/modal/todoReminder/schema.ts
@@ -5,7 +5,7 @@ type EditTodoItemFormSchema = z.infer<typeof editTodoItemFormSchema>;
 const editTodoItemFormSchema = z.object({
 	content: z.string({ required_error: 'Necessarily in need' }).min(1, { message: 'Write Content' }),
 	tags: z.array(z.object({ id: z.number(), tag: z.string() })).default([]),
-	reminderTime: z.date({ invalid_type_error: 'Select the correct Data' }),
+	reminderTime: z.date({ required_error: 'Select Date in essential', invalid_type_error: 'Select the correct Data' }),
 });
 
 export type { EditTodoItemFormSchema };
