b2c655071e390337948d3b16fede71092dc3f119
olhkyle <kylekwon.dev@gmail.com>
2025-03-20 18:24:49 +0900

    feat : develop `useTogglePaymentIsFixedMutation` to toggle `isFixed` on `ExpenseTracker` state
    - develop `Switch.tsx` UI
    - use `debounce` to prevent user from toggling server state continuously and stop to send unnecessary request

--- src/components/common/Switch.tsx
+++ src/components/common/Switch.tsx
@@ -1,20 +1,27 @@
 import { useState } from 'react';
 import styled from '@emotion/styled';
+import { debounce } from 'es-toolkit';
 
 interface SwitchProps {
 	initialValue: boolean;
+	toggle: (newState: boolean) => void;
 }
 
-const Switch = ({ initialValue }: SwitchProps) => {
-	const [isToggle, setIsToggle] = useState(initialValue);
+const Switch = ({ initialValue, toggle }: SwitchProps) => {
+	const [isActive, setIsActive] = useState(initialValue);
+
+	const handleSwitchToggle = (newState: boolean) => {
+		setIsActive(newState);
+
+		const debouncedServerUpdate = debounce(isActive => toggle(isActive), 200);
+
+		debouncedServerUpdate(newState);
+	};
 
 	return (
-		<Container
-			onClick={() => {
-				setIsToggle(!isToggle);
-			}}>
-			<Trigger isToggle={isToggle} />
-			<Background isToggle={isToggle} />
+		<Container onClick={() => handleSwitchToggle(!isActive)}>
+			<Trigger isActive={isActive} />
+			<Background isActive={isActive} />
 		</Container>
 	);
 };
@@ -26,10 +33,10 @@ const Container = styled.div`
 	cursor: pointer;
 `;
 
-const Trigger = styled.button<{ isToggle: boolean }>`
+const Trigger = styled.button<{ isActive: boolean }>`
 	position: absolute;
 	top: 3px;
-	left: ${({ isToggle }) => (isToggle ? '26px' : '2px')};
+	left: ${({ isActive }) => (isActive ? '26px' : '2px')};
 	width: 20px;
 	height: 20px;
 	background-color: var(--white);
@@ -38,10 +45,10 @@ const Trigger = styled.button<{ isToggle: boolean }>`
 	transition: left calc(0.2 * 1s);
 `;
 
-const Background = styled.div<{ isToggle: boolean }>`
+const Background = styled.div<{ isActive: boolean }>`
 	display: flex;
 	height: 100%;
-	background-color: ${({ isToggle }) => (isToggle ? 'var(--black)' : 'var(--grey300)')};
+	background-color: ${({ isActive }) => (isActive ? 'var(--black)' : 'var(--grey300)')};
 	border-radius: var(--radius-extra);
 	box-shadow: 2px 2px 5px 0 rgba(50, 50, 50, 0.25);
 	transition: background calc(0.2 * 1s);
---
+++ src/hooks/mutations/expenseTracker/useTogglePaymentIsFixedMutation.ts
@@ -0,0 +1,59 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { ExpenseTracker, PaymentsByDate, togglePaymentIsFixed } from '../../../supabase';
+import { useToastStore } from '../../../store';
+import { toastData, queryKey } from '../../../constants';
+import { formatByKoreanTime } from '../../../utils';
+
+interface UseTogglePaymentIsFixedMutation {
+	currentDate: Date;
+	handlers: { [key: string]: () => void };
+}
+
+type Variables = Pick<ExpenseTracker, 'id' | 'isFixed' | 'updated_at'>;
+
+const toggle =
+	({ id, isFixed }: Variables) =>
+	(oldData: PaymentsByDate) => {
+		return { ...oldData, expense: oldData.expense.map(item => (item.id === id ? { ...item, isFixed } : item)) };
+	};
+
+const useTogglePaymentIsFixedMutation = ({ currentDate, handlers: { goBack } }: UseTogglePaymentIsFixedMutation) => {
+	const queryClient = useQueryClient();
+	const { addToast } = useToastStore();
+	const QUERY_KEY = [...queryKey.EXPENSE_TRACKER, formatByKoreanTime(currentDate)];
+
+	return useMutation({
+		async mutationFn(variables: Variables) {
+			await togglePaymentIsFixed(variables);
+		},
+		async onMutate(variables) {
+			await queryClient.cancelQueries({ queryKey: QUERY_KEY });
+
+			const previousData = queryClient.getQueryData(QUERY_KEY);
+
+			if (previousData) {
+				queryClient.setQueryData(QUERY_KEY, toggle(variables));
+			}
+
+			return { previousData };
+		},
+
+		onSuccess() {
+			addToast(toastData.EXPENSE_TRACKER.TOGGLE.SUCCESS);
+			goBack();
+		},
+
+		onError(error, _, context) {
+			if (context?.previousData) {
+				console.error(error);
+				queryClient.setQueryData(QUERY_KEY, context?.previousData);
+				addToast(toastData.EXPENSE_TRACKER.TOGGLE.ERROR);
+			}
+		},
+		onSettled() {
+			return queryClient.invalidateQueries({ queryKey: QUERY_KEY });
+		},
+	});
+};
+
+export default useTogglePaymentIsFixedMutation;
