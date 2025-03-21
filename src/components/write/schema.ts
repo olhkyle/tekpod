29c2a530628868afe78402ba61560d6b4aa0a863
olhkyle <kylekwon.dev@gmail.com>
2025-03-21 16:48:26 +0900

    - refactor: change one sentence to use a reusable constant and remove `default` method
    - no need to add a default method for the tags property
    - tags already has default values in React Hook Form

--- src/components/write/schema.ts
+++ src/components/write/schema.ts
@@ -2,11 +2,13 @@ import { z } from 'zod';
 
 type WriteSchema = z.infer<typeof writeSchema>;
 
+const REQUIRED_ERROR_MESSAGE = 'Necessarily in need';
+
 const writeSchema = z.object({
-	title: z.string({ required_error: 'Necessarily in need' }).min(2, { message: 'Write a title' }),
-	content: z.string({ required_error: 'Necessarily in need' }).min(1, { message: 'Write content' }),
-	feeling: z.string({ required_error: 'Necessarily in need' }).min(1, { message: 'Write one feeling' }),
-	tags: z.array(z.object({ id: z.number(), tag: z.string() })).default([]),
+	title: z.string({ required_error: REQUIRED_ERROR_MESSAGE }).min(2, { message: 'Write a title' }),
+	content: z.string({ required_error: REQUIRED_ERROR_MESSAGE }).min(1, { message: 'Write content' }),
+	feeling: z.string({ required_error: REQUIRED_ERROR_MESSAGE }).min(1, { message: 'Write one feeling' }),
+	tags: z.array(z.object({ id: z.number(), tag: z.string() })),
 });
 
 export type { WriteSchema };
