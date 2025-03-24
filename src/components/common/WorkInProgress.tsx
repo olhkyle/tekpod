bdf89852455a6b511384114c6ea0fb6737d74451
olhkyle <kylekwon.dev@gmail.com>
2025-03-24 16:46:18 +0900

    style : use `css prop` instead of styled

--- src/components/common/WorkInProgress.tsx
+++ src/components/common/WorkInProgress.tsx
@@ -1,18 +1,19 @@
-import styled from '@emotion/styled';
-
 const WorkInProgress = () => {
-	return <Container>WorkInProgress 🛞</Container>;
+	return (
+		<div
+			css={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+				minHeight: '200px',
+				backgroundColor: 'var(--grey50)',
+				fontWeight: 'var(--black)',
+				border: '1px solid var(--grey100)',
+				borderRadius: 'var(--radius-l)',
+			}}>
+			WorkInProgress 🛞
+		</div>
+	);
 };
-
-const Container = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	min-height: 200px;
-	background-color: var(--grey50);
-	font-weight: var(--black);
-	border: 1px solid var(--grey100);
-	border-radius: var(--radius-l);
-`;
 
 export default WorkInProgress;
