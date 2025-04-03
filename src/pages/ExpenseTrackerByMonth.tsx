1200cd450f4497fe93e478b320a414a96de57cee
olhkyle <kylekwon.dev@gmail.com>
2025-04-03 12:12:44 +0900

    fix : fix error of `state.currentDate` being null

--- src/pages/ExpenseTrackerByMonth.tsx
+++ src/pages/ExpenseTrackerByMonth.tsx
@@ -10,10 +10,8 @@ export type ExtendedPaymentMethodType = (typeof segmentedControlOptions)[number]
 const segmentedControlOptions = ['All', 'Card', 'Cash'] as const;
 
 const ExpenseTrackerByMonthPage = () => {
-	const {
-		state: { currentDate },
-	} = useLocation() as { state: { currentDate: Date } };
-	const [selected, setSelected] = useState<Date>(currentDate ?? today);
+	const { state } = useLocation() as { state: { currentDate: Date } };
+	const [selected, setSelected] = useState<Date>(state?.currentDate ?? today);
 	const { setModal } = useModalStore();
 
 	const [currentPaymentMethod, setCurrentPaymentMethod] = useState<ExtendedPaymentMethodType>(segmentedControlOptions[0]);
