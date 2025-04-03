fcfa4b4ca32e3b5245f41fd362e3215878361f5b
olhkyle <kylekwon.dev@gmail.com>
2025-04-03 19:10:01 +0900

    feat : modify `loader` for `DiaryContent` and TodoList

--- src/components/todoReminder/TodoListLoader.tsx
+++ src/components/todoReminder/TodoListLoader.tsx
@@ -3,8 +3,8 @@ import { SkeletonLoader } from '..';
 const TodoListLoader = () => {
 	return (
 		<div css={{ display: 'flex', flexDirection: 'column', gap: '16px', marginTop: '32px' }}>
-			{Array.from({ length: 3 }, (_, idx) => (
-				<SkeletonLoader key={idx} theme={'light'} width={'100%'} height={'32px'} />
+			{Array.from({ length: 5 }, (_, idx) => (
+				<SkeletonLoader key={idx} theme={'light'} width={'100%'} height={'60px'} />
 			))}
 		</div>
 	);
