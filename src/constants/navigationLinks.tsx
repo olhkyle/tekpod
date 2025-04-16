567349c387de5fc36b2594519b39237f7b1c5dff
olhkyle <kylekwon.dev@gmail.com>
2025-04-16 16:59:17 +0900

    feat : add `"commute"` route for developing new page

--- src/constants/navigationLinks.tsx
+++ src/constants/navigationLinks.tsx
@@ -1,28 +1,34 @@
 import { IoMdNotifications } from 'react-icons/io';
 import { FaCalculator } from 'react-icons/fa';
 import { RiBook3Line, RiCamera3Line } from 'react-icons/ri';
+import { SiOnlyoffice } from 'react-icons/si';
 import { routes } from '.';
 
 const navigationLinks = [
 	{
 		to: routes.TODO_REMINDER,
-		icon: <IoMdNotifications size="24" color="var(--white)" />,
-		title: 'Tasks to do',
+		icon: <IoMdNotifications size="36" color="var(--white)" />,
+		title: 'Todo',
 	},
 	{
 		to: routes.EXPENSE_TRACKER,
-		icon: <FaCalculator size="18" color="var(--blue100)" />,
-		title: 'Expense Tracker',
+		icon: <FaCalculator size="32" color="var(--blue100)" />,
+		title: 'Expense',
 	},
 	{
 		to: routes.FILM_RECIPE,
-		icon: <RiCamera3Line size="22" color="var(--blue100)" />,
-		title: 'Film Recipe',
+		icon: <RiCamera3Line size="40" color="var(--blue100)" />,
+		title: 'Recipe',
 	},
 	{
 		to: routes.DIARY,
-		icon: <RiBook3Line size="21" color="var(--blue100)" />,
+		icon: <RiBook3Line size="34" color="var(--blue100)" />,
 		title: 'Diary',
+	},
+	{
+		to: routes.COMMUTE_TRACKER,
+		icon: <SiOnlyoffice size="34" color="var(--blue100)" />,
+		title: 'Commute',
 	},
 ] as const;
 
