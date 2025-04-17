bfbb35a3c9bc76e3ede01c7b8e19de6f0e3ba8fe
olhkyle <kylekwon.dev@gmail.com>
2025-04-17 16:11:09 +0900

    style : change style property of `SkeletonLoader`

--- src/components/common/SkeletonLoader.tsx
+++ src/components/common/SkeletonLoader.tsx
@@ -3,27 +3,13 @@ import styled from '@emotion/styled';
 const SkeletonLoader = styled.div<{
 	width: `${number}px` | `${number}%`;
 	height: `${number}px` | `${number}%`;
-	theme: 'light' | 'dark';
 }>`
-	--linear-gradient: linear-gradient(to right, var(--blue100), var(--grey100), var(--blue100));
-
 	position: relative;
 	width: ${({ width }) => width};
 	height: ${({ height }) => height};
-	background-color: ${({ theme }) =>
-		theme === 'light' ? 'var(--greyOpacity50)' : theme === 'dark' ? 'var(--grey800)' : 'var(--greyOpactiy50)'};
+	background-color: var(--greyOpacity50);
 	border-radius: var(--radius-s);
 	overflow: hidden;
-
-	@keyframes loading {
-		0% {
-			transform: translate3d(0, 0, 0);
-		}
-		50%,
-		100% {
-			transform: translate3d(450px, 0, 0);
-		}
-	}
 
 	@keyframes shimmer {
 		0% {
@@ -40,7 +26,7 @@ const SkeletonLoader = styled.div<{
 		top: 0;
 		left: 0;
 		inset: 0;
-		background: linear-gradient(90deg, transparent 0%, var(--blue100) 50%, transparent 100%);
+		background: linear-gradient(90deg, transparent 0%, var(--grey100) 50%, transparent 100%);
 		background-size: 200% 100%;
 		animation: shimmer 2s infinite;
 	}
