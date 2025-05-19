0e8af0bdbd2c43a55aaa0c4f01b9f33bf6c33621
olhkyle <kylekwon.dev@gmail.com>
2025-05-19 16:02:33 +0900

    feat : add Flex component on `index.ts` strategy

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -6,6 +6,7 @@ export { default as DatePicker } from './DatePicker';
 export { default as Description } from './Description';
 export { default as Drawer } from './Drawer';
 export { default as EmptyMessage } from './EmptyMessage';
+export { default as Flex } from './Flex';
 export { default as LazyImage } from './LazyImage';
 export { default as Logo } from './Logo';
 export { default as SegmentedControl } from './SegmentedControl';
